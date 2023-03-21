2fb10eaeb36ebd4840bbdcf842c66914614a81cb
liuup <akarealup@gmail.com>
2023-03-21 19:41:42 +0800

    添加websocket

---
+++ netapi/packet.go
@@ -0,0 +1,13 @@
+package netapi
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+// 使用默认配置
+var upgrader = websocket.Upgrader{}
+
+func GetPakcet(c *gin.Context) {
+
+}
